aebbca19ecd39b496f1c38bd77fbc3c07063185b
Harsh Sahu <yesh8harsh+github@gmail.com>
2025-10-11 16:36:19 +0530

    chore[INIT]: bootstrapping nextra

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,5 +1,17 @@
 import { SofiaProSoft } from "@zephyr/ui";
 import type { Metadata } from "next";
+import { Head } from "nextra/components";
+import { getPageMap } from "nextra/page-map";
+import { Layout, Navbar } from "nextra-theme-docs";
+import "nextra-theme-docs/style.css";
+import Image from "next/image";
+import "./styles.css";
+
+const navbar = (
+  <Navbar
+    logo={<Image alt="Zephyr Logo" height={44} src="/zeph.png" width={44} />}
+  />
+);
 
 export const metadata: Metadata = {
   title: "Zephyr Docs",
@@ -9,7 +21,7 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -17,9 +29,23 @@ export default function RootLayout({
   return (
     <html
       className={`${SofiaProSoft.className} ${SofiaProSoft.variable} antialiased`}
+      dir="ltr"
       lang="en"
+      suppressHydrationWarning
     >
-      <body className={"antialiased"}>{children}</body>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <body>
+        <Layout
+          docsRepositoryBase="https://github.com/zephverse/zephyr/tree/main/apps/docs"
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          sidebar={{ autoCollapse: true }}
+        >
+          {children}
+        </Layout>
+      </body>
     </html>
   );
 }
--- apps/docs/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to Zephyr Docs</h1>
-    </div>
-  );
-}
