f11a805c855b5c197f70d9c55d5d79e6bb308a9b
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-04-20 12:58:32 +0530

    fix[RUN]: fix runtime error

--- apps/docs/src/app/[[...mdxPath]]/page.jsx
+++ apps/docs/src/app/[[...mdxPath]]/page.jsx
@@ -2,6 +2,7 @@ import { generateStaticParamsFor, importPage } from "nextra/pages";
 import { useMDXComponents as getMDXComponents } from "../../mdx-components";
 
 export const generateStaticParams = generateStaticParamsFor("mdxPath");
+export const dynamicParams = false;
 
 export async function generateMetadata(props) {
   const params = await props.params;
